15078b94c89e5e1a80e9d26e02a2905a4d0fc96a
Guilherme <guilhermesaldanha08@gmail.com>
2021-05-25 12:49:51 -0300

    adição de graficos

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ const config = {
   );
 
 
-  var ctx = document.getElementById('myChart2');
+var ctx = document.getElementById('myChart2');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -67,3 +67,30 @@ var myChart = new Chart(ctx, {
         }
     }
 });
+
+
+
+// Chart Donout
+
+var myChart = new Chart(
+    document.getElementById('myChart3'),{
+        type: 'doughnut',
+        data: {
+            labels: [
+              'Red',
+              'Blue',
+              'Yellow'
+            ],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [300, 50, 100],
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+              hoverOffset: 4
+            }]
+          }
+    }
+  );
