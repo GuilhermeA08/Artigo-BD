cf3c2dad5a6dfb2ee0423d17a76017c622c558c2
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2021-07-11 09:36:16 -0300

    fix: change charts on dashboard

--- js/chart_config.js
+++ js/chart_config.js
@@ -1,42 +1,36 @@
 (async () => {
-  //Chart Line
-
   // Select dados
   const chart_line2 = await Select_chart_line();
 
   let teste = chart_line2.map((elemento) => parseInt(elemento.confirmados));
   let teste2 = chart_line2.map((elemento) => parseInt(elemento.curados));
   let teste3 = chart_line2.map((elemento) => parseInt(elemento.obitos));
-  console.log(teste);
-
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Confirmados',
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        data: teste,
-      },
-      {
-        label: 'Curados',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        data: teste2,
-      },
-      {
-        label: 'Mortes',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        data: teste3,
-      },
-    ],
-  };
 
   const config = {
     type: 'line',
-    data,
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          label: 'Confirmados',
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          data: teste,
+        },
+        {
+          label: 'Curados',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          data: teste2,
+        },
+        {
+          label: 'Mortes',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          data: teste3,
+        },
+      ],
+    },
     options: {},
   };
 
@@ -47,59 +41,41 @@
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+      labels: ['teste', 'ola', '1', '2', 4, 5],
       datasets: [
         {
-          label: 'Infectados por mês',
-          data: [33, 19, 43, 50, 27, 39],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
+          label: 'Dataset 1',
+          data: [1, 2, 3, 4, 5, 6],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 1)',
+        },
+        {
+          label: 'Dataset 2',
+          data: [7, 6, 4, 2, 5, 8],
+          borderColor: 'rgba(255, 99, 132, 1)',
+          backgroundColor: 'rgba(255, 99, 132, 1)',
         },
       ],
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true,
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Bar Chart',
         },
       },
-    },
-  });
-
-  // Chart Donout
-
-  var myChart = new Chart(document.getElementById('myChart3'), {
-    type: 'doughnut',
-    data: {
-      labels: ['Hospitalizados', 'Isolamento', 'Curados', 'Obitos'],
-      datasets: [
-        {
-          label: 'CONFIRMADOS',
-          data: [3, 41, 440, 11],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)',
-            'rgb(153, 102, 255)',
-          ],
-          hoverOffset: 4,
+      scales: {
+        x: {
+          stacked: true,
         },
-      ],
+        y: {
+          stacked: true,
+        },
+      },
     },
   });
 })();
--- js/table.js
+++ js/table.js
@@ -1,60 +1,77 @@
+function SelectAllData() {
+  firebase
+    .database()
+    .ref('dias')
+    .once('value', function (AllRecords) {
+      AllRecords.forEach(function (CurrentRecord) {
+        var notificados_var = CurrentRecord.val().notificados;
+        var suspeitos_var = CurrentRecord.val().suspeitos;
+        var descartados_var = CurrentRecord.val().descartados;
+        var confirmados_var = CurrentRecord.val().confirmados;
+        var hospitalizados_var = CurrentRecord.val().hospitalizados;
+        var tratamento_var = CurrentRecord.val().tratamento;
+        var curados_var = CurrentRecord.val().curados;
+        var obitos_var = CurrentRecord.val().obitos;
+        var dia_var = CurrentRecord.val().dia;
 
- function SelectAllData()
-{
-    firebase.database().ref('dias').once('value',
-        function(AllRecords){
-        AllRecords.forEach(function(CurrentRecord){
-                    var notificados_var = CurrentRecord.val().notificados;
-                    var suspeitos_var = CurrentRecord.val().suspeitos;
-                    var descartados_var = CurrentRecord.val().descartados;
-                    var confirmados_var = CurrentRecord.val().confirmados;
-                    var hospitalizados_var = CurrentRecord.val().hospitalizados;
-                    var tratamento_var = CurrentRecord.val().tratamento;
-                    var curados_var = CurrentRecord.val().curados;
-                    var obitos_var = CurrentRecord.val().obitos;
-                    var dia_var = CurrentRecord.val().dia;
-
-                    AddItensToTable(notificados_var, suspeitos_var, descartados_var, confirmados_var, hospitalizados_var, tratamento_var, curados_var, obitos_var, dia_var);
-                }
-            );
-        }
-    );
+        AddItensToTable(
+          notificados_var,
+          suspeitos_var,
+          descartados_var,
+          confirmados_var,
+          hospitalizados_var,
+          tratamento_var,
+          curados_var,
+          obitos_var,
+          dia_var
+        );
+      });
+    });
 }
 
 window.onload = SelectAllData;
- function AddItensToTable(notificados, suspeitos, descartados, confirmados, hospitalizados, tratamento, curados, obitos, dias){
-    var tbody = document.getElementById('tbody1');
-    var trow = document.createElement('tr');
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
-    var td4 = document.createElement('td');
-    var td5 = document.createElement('td');
-    var td6 = document.createElement('td');
-    var td7 = document.createElement('td');
-    var td8 = document.createElement('td');
-    var td9 = document.createElement('td');
+function AddItensToTable(
+  notificados,
+  suspeitos,
+  descartados,
+  confirmados,
+  hospitalizados,
+  tratamento,
+  curados,
+  obitos,
+  dias
+) {
+  var tbody = document.getElementById('tbody1');
+  var trow = document.createElement('tr');
+  var td1 = document.createElement('td');
+  var td2 = document.createElement('td');
+  var td3 = document.createElement('td');
+  var td4 = document.createElement('td');
+  var td5 = document.createElement('td');
+  var td6 = document.createElement('td');
+  var td7 = document.createElement('td');
+  var td8 = document.createElement('td');
+  var td9 = document.createElement('td');
 
-    td1.innerHTML = notificados;
-    td2.innerHTML = suspeitos;
-    td3.innerHTML = descartados;
-    td4.innerHTML = confirmados;
-    td5.innerHTML = hospitalizados;
-    td6.innerHTML = tratamento;
-    td7.innerHTML = curados;
-    td8.innerHTML = obitos;
-    td9.innerHTML = dias;
+  td9.innerHTML = notificados;
+  td2.innerHTML = suspeitos;
+  td3.innerHTML = descartados;
+  td4.innerHTML = confirmados;
+  td5.innerHTML = hospitalizados;
+  td6.innerHTML = tratamento;
+  td7.innerHTML = curados;
+  td8.innerHTML = obitos;
+  td1.innerHTML = dias;
 
-    trow.appendChild(td1);
-    trow.appendChild(td2);
-    trow.appendChild(td3);
-    trow.appendChild(td4);
-    trow.appendChild(td5);
-    trow.appendChild(td6);
-    trow.appendChild(td7);
-    trow.appendChild(td8);
-    trow.appendChild(td9);
+  trow.appendChild(td1);
+  trow.appendChild(td2);
+  trow.appendChild(td3);
+  trow.appendChild(td4);
+  trow.appendChild(td5);
+  trow.appendChild(td6);
+  trow.appendChild(td7);
+  trow.appendChild(td8);
+  trow.appendChild(td9);
 
-    tbody.appendChild(trow);
+  tbody.appendChild(trow);
 }
-
